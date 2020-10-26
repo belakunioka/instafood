d7d254c7dec98e1e3af51e324e543c775425a901
Rayssa Ferreira <rayssaferr98@gmail.com>
2020-10-26 11:27:22 -0300

    ajuste nos caminhos do menu superior

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ $("body").prepend(
     '<div class="nav-bar">' +
       '<div class="nav-link">'+
           '<ul>'+
-              '<li><a href="/"><img src="src/assets/img/logo.png" id="logo"</a></li>' +
-              '<li><a href="/">MENU PRINCIPAL</a></li>' +
-              '<li><a href="/">RECEITAS SALGADAS</a></li>'+
-              '<li><a href="/">RECEITAS DOCES</a></li>'+
+              '<li><a href=""><img src="src/assets/img/logo.png" id="logo"</a></li>' +
+              '<li><a href="">MENU PRINCIPAL</a></li>' +
+              '<li><a href="">RECEITAS SALGADAS</a></li>'+
+              '<li><a href="">RECEITAS DOCES</a></li>'+
           '</ul>'+
           '<a href="src/templates/cadastro.html"><button type="button" class="btn-vermelho"> SE INSCREVA </button></a>'+
       '</div>'+
@@ -17,8 +17,8 @@ $("body").prepend(
           "<i class='fa fa-search'></i>" +
           "<div class='search-icon'>" +
               "<ul>" +
-                  "<li><a href='/' target='_self'>RECEITAS <br>SALVAS</a></li>" +
-                  "<li><a href='/' target='_self'>POSTAR</a></li>" +
+                  "<li><a href='' target='_self'>RECEITAS <br>SALVAS</a></li>" +
+                  "<li><a href='' target='_self'>POSTAR</a></li>" +
                   "<li><a href='src/templates/login.html' target='_self'>ENTRAR</a>" +
                   "</li>" +
               "</ul>" +
