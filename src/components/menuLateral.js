452d2e7853daa9994dfaab21806509050d6562b8
Rayssa Ferreira <rayssaferr98@gmail.com>
2020-10-26 09:15:23 -0300

    pequena identação

--- src/components/menuLateral.js
+++ src/components/menuLateral.js
@@ -1,22 +1,24 @@
-$("body").prepend("<div class='vertical-bar'>" +
-    "<div class='search-icon'>" +
-    "<a href='/'><i class='fa fa-th-list'></i></a>" +
-    "<i class='fa fa-search'></i>" +
-    "<div class='search-icon'>" +
-    "<ul>" +
-    "<li><a href='/' target='_blank'>RECEITAS <br>SALVAS</a></li>" +
-    "<li><a href='/' target='_blank'>POSTAR</a></li>" +
-    "<li><a href='../login/index.html' target='_self'>ENTRAR</a>" +
-    "</li>" +
-    "</ul>" +
-    "</div>" +
-    "</div>" +
-    "<div class='social-icons'>" +
-    "<ul>" +
-    "<li><i class='fab fa-facebook-f fa-2x icon-social'></i></li>" +
-    "<li><i href='/' class='fab fa-twitter fa-2x icon-social'></i></li>" +
-    "<li><i href='/' class='fab fa-pinterest fa-2x icon-social'></i></li>" +
-    "<li><i href='/' class='fab fa-instagram fa-2x icon-social'></i></li>" +
-    "</ul>" +
-    "</div>" +
-    "</div>")
+$("body").prepend(
+    "<div class='vertical-bar'>" +
+        "<div class='search-icon'>" +
+            "<a href='/'><i class='fa fa-th-list'></i></a>" +
+            "<i class='fa fa-search'></i>" +
+            "<div class='search-icon'>" +
+                "<ul>" +
+                    "<li><a href='/' target='_self'>RECEITAS <br>SALVAS</a></li>" +
+                    "<li><a href='/' target='_self'>POSTAR</a></li>" +
+                    "<li><a href='../templates/login.html' target='_self'>ENTRAR</a>" +
+                    "</li>" +
+                "</ul>" +
+            "</div>" +
+        "</div>" +
+        "<div class='social-icons'>" +
+            "<ul>" +
+                "<li><i class='fab fa-facebook-f fa-2x icon-social'></i></li>" +
+                "<li><i href='/' class='fab fa-twitter fa-2x icon-social'></i></li>" +
+                "<li><i href='/' class='fab fa-pinterest fa-2x icon-social'></i></li>" +
+                "<li><i href='/' class='fab fa-instagram fa-2x icon-social'></i></li>" +
+            "</ul>" +
+        "</div>" +
+    "</div>"+
+    "<div class='nav-bar'>")
--- src/components/menuSuperior.js
+++ src/components/menuSuperior.js
@@ -1,17 +1,12 @@
-$("body").prepend('<div class="nav-bar">' +
-'<div class="nav-link">'+
-' <ul>'+
-'   <li><img src="../assets/img/logo.png" id="logo"></li>' +
-'   <a href="/">'+
-'     <li>MENU PRINCIPAL</li>' +
-'   </a>'+
-'   <a href="/">'+
-'     <li>RECEITAS SALGADAS</li>'+
-'   </a>'+
-'   <a href="/">'+
-'     <li>RECEITAS DOCES</li>'+
-'   </a>'+
-' </ul>'+
- '<a href="src/templates/login.html" target="_self"><button type="button" class="btn-vermelho"> SE INSCREVA </button></a>'+
-  '</div>'+
-'</div>')
+$("body").prepend(
+  '<div class="nav-bar">' +
+      '<div class="nav-link">'+
+          '<ul>'+
+              '<li><a href="/"><img src="../assets/img/logo.png" id="logo"</a></li>' +
+              '<li><a href="/">MENU PRINCIPAL</a></li>' +
+              '<li><a href="/">RECEITAS SALGADAS</a></li>'+
+              '<li><a href="/">RECEITAS DOCES</a></li>'+
+          '</ul>'+
+          '<a href="../templates/cadastro.html"><button type="button" class="btn-vermelho"> SE INSCREVA </button></a>'+
+      '</div>'+
+  '</div>')
